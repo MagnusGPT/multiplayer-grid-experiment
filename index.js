43d15d74e01a43353d99c2edb49d0160a2a2c5d3
Magnus The Developer <95050512+MagnusGPT@users.noreply.github.com>
2026-03-17 04:56:24 +0100

    Added players. Now they move.

--- index.js
+++ index.js
@@ -7,13 +7,67 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
+let world = {
+    grid: Array.from({ length: 50 }, () => Array(50).fill(0)), //initial array
+    users: {}
+}
+let worldTick = 0;
+
+
 app.get('/', (req, res) => {
     res.sendFile('./public/index.html')
 });
 
 io.on('connection', (socket) => {
     console.log(socket.id, 'connected.');
+    world.users[socket.id] = {
+        x: 25,
+        y: 25
+    }
+
+    socket.on('playerRight', ()=>{
+        if (world.users[socket.id].x < 50) {
+            world.grid[world.users[socket.id].y][world.users[socket.id].x] = 0;
+            world.users[socket.id].x++;
+        }
+    });
+
+    socket.on('playerLeft', ()=>{
+        if (world.users[socket.id].x > 0) {
+            world.grid[world.users[socket.id].y][world.users[socket.id].x] = 0;
+            world.users[socket.id].x--;
+        }
+    });
+
+    socket.on('playerUp', ()=>{
+        if (world.users[socket.id].y < 50) {
+            world.grid[world.users[socket.id].y][world.users[socket.id].x] = 0;
+            world.users[socket.id].y++;
+        }
+    });
+
+    socket.on('playerDown', ()=>{
+        if (world.users[socket.id].y > 0) {
+            world.grid[world.users[socket.id].y][world.users[socket.id].x] = 0;
+            world.users[socket.id].y--;
+        }
+    });
 });
+
+function setPlayerPositions() {
+    if (world.users.length !== 0) {
+        for (let users in world.users) {
+            world.grid[world.users[users].y][world.users[users].x] = users;
+        }
+    }
+}
+
+setInterval(() => {
+    worldTick++;
+    setPlayerPositions();
+    io.emit("tick", worldTick);
+    io.emit("updateWorld", world);
+}, 50);
 
 server.listen(3000, () => {
     console.log('Server open at http://localhost:3000');
--- public/script.js
+++ public/script.js
@@ -1,1 +1,37 @@
 const socket = io();
+
+const container = document.getElementById('grid-container');
+
+function renderGrid(data) {
+  // Clear the container first
+  container.innerHTML = ''; 
+
+  // Loop through rows
+  for (let r = 49; r >= 0; r--) {
+    // Loop through columns
+    for (let c = 0; c < 50; c++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+
+      // If the array value is 1, make it look "active"
+      if (data[r][c] !== 0) {
+        cell.classList.add('active');
+      }
+
+      container.appendChild(cell);
+    }
+  }
+}
+
+window.addEventListener('keydown', (event) => {
+    const key = event.key.toLowerCase();
+
+    if (key === 'w') socket.emit("playerUp");
+    if (key === 'a') socket.emit("playerLeft");
+    if (key === 's') socket.emit("playerDown");
+    if (key === 'd') socket.emit("playerRight");
+});
+
+socket.on("updateWorld", (world) => {
+    renderGrid(world.grid);
+});
